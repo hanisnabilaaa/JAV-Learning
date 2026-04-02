0bfb60c30e7476142499f9aa13d8df17c8628f75
Hanis Nabila <habd0035@student.monash.edu>
2026-04-02 08:26:17 +0800

    Learning if else statement. Have to end with else or the output will not come out

---
+++ Week4/Tutorial4/Question1.java
@@ -0,0 +1,14 @@
+///a. Determine whether 3x8=27.
+
+public class Question1 {
+    public static void main(String[] args) {
+        ///int a = 3;
+        ///int b = 8;
+        ///int c = a*b;
+        if (3*8 == 27){
+            System.out.println("True");
+        }else {
+            System.out.println("False");
+        }
+    }
+}
